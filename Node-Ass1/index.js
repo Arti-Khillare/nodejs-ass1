894098a7fdc940a3449db5f01b0d7fae81b34e85
Arti-Khillare <agkhillare@gmail.com>
2023-07-17 12:34:29 +0530

    adding changes in index.js file

--- Node-Ass1/index.js
+++ Node-Ass1/index.js
@@ -2,16 +2,37 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const createHttpError = require('http-errors');
+
+/**
+ * creating seperate variable for express function
+ */
 const app = express();
+
+/**
+ * requiring dotenv file for configuration
+ */
 require('dotenv').config();
+
+/**
+ * storing port value from env
+ */
 const PORT = process.env.PORT || 3000;
 
+/**
+ * cross origin resource sharing package, it is used to specify which origin can access api
+ */
+const cors = require('cors');
+app.use(cors({
+    origin:'*'
+}));
+
+/**
+ * parsing the data res.body in json format using bodyParser
+ */
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.get('/hello', (req, res) => {
-    res.send("heelos api")
-})
+
 app.use('/api/auth', require('./src/routes/authRoute'));
 app.use('/api/users', require('./src/routes/userRoute'));
 app.use('/api/products', require('./src/routes/productsRoute'));
@@ -29,13 +50,17 @@ app.use((error, req, res, next) => {
  });
 });
 
+/**
+ * connecting mongoose
+ */
 mongoose.connect(process.env.DB_CONNECT , {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(console.log("connection successfull"));
 
+/**
+ * server app is listening on port
+ */
 app.listen(PORT, () => {
     console.log(`Server is listening on port : ${PORT}`);
 })
-// $env:NODE_ENV="development"  
-//set the environment variable first
